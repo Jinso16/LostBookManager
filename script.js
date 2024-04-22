b4de4d7b8a6fd4bfba260203a25545041b8773e7
João Vitor Michelon <joaovitormichelon17@gmail.com>
2024-04-22 15:20:24 -0300

    1° commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const formulario = document.getElementById('formulario');
+
+formulario.addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita o envio padrão do formulário
+
+  const titulo = document.getElementById('titulo').value;
+  const autor = document.getElementById('autor').value;
+  const genero = document.getElementById('genero').value;
+  const estado = document.getElementById('estado').value;
+  const nomeDoador = document.getElementById('nomeDoador').value;
+  const contatoDoador = document.getElementById('contatoDoador').value;
+
+  // Validação opcional (evita envio de dados incompletos)
+  if (titulo === '' || autor === '' || genero === '' || estado === '') {
+    alert('Preencha todos os campos obrigatórios!');
+    return; // Interrompe a execução do código caso a validação falhe
+  }
+
+  // Salvar no navegador
+  localStorag
+
+  alert('Livro cadastrado com sucesso!');
+});
